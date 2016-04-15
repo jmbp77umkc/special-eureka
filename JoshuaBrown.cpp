2c7f2b00386f256ae137073600221fbedb347e1e
jorh <jorh@localhost.localdomain>
2016-04-15 16:11:25 -0500

    I wuz heer!

---
+++ JoshuaBrown.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   JoshuaBrown.cpp
+ * Author: jorh
+ * 
+ * Created on April 15, 2016, 4:10 PM
+ */
+
+#include "JoshuaBrown.h"
+
+JoshuaBrown::JoshuaBrown() {
+}
+
+JoshuaBrown::JoshuaBrown(const JoshuaBrown& orig) {
+}
+
+JoshuaBrown::~JoshuaBrown() {
+}
+
---
+++ JoshuaBrown.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   JoshuaBrown.h
+ * Author: jorh
+ *
+ * Created on April 15, 2016, 4:10 PM
+ */
+
+#ifndef JOSHUABROWN_H
+#define JOSHUABROWN_H
+
+class JoshuaBrown {
+public:
+    JoshuaBrown();
+    JoshuaBrown(const JoshuaBrown& orig);
+    virtual ~JoshuaBrown();
+private:
+
+};
+
+#endif /* JOSHUABROWN_H */
+
